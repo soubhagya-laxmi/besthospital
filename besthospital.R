361dfca5d94920bac2cb43fb1957b3e53d841c93
soubhagya-laxmi <shubbi.tripathy@gmail.com>
2016-09-29 11:27:52 +0530

    Update besthospital.R

--- besthospital.R
+++ besthospital.R
@@ -1,9 +1,16 @@
+## best function take two arguments: name of a state and an outcome name
+## The function reads the outcome-of-care-measures.csv
+## returns a character vector with the name of the hospital 
+## The hospital that has the best (i.e. lowest) 30-day mortality for the specified outcome in that state.
+
+## The outcomes can be one of "heart attack", "heart failure", or "pneumonia".
 
 best <- function(state, outcome)
 {
-  
+  ## Read outcome data
   dat<- read.csv("outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
   
+  ## Check that state and outcome are valid
   validOutcome <- c("heart attack","heart failure","pneumonia")
   if(!outcome %in% validOutcome)
   { stop("Invalid Outcome") }
@@ -19,6 +26,7 @@ best <- function(state, outcome)
   k <- dat[dat$State==state,]
   idx <- which.min(as.double(k[,colName]))
   
+  ## Return hospital name in that state with lowest 30-day death rate
   k[idx,"Hospital.Name"]
 }
 
